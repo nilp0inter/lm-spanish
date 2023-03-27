f4b001279c74795226bc617e053d8d5f2fecb7dc
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2023-03-27 18:39:18 +0200

    chore: try to use cude

--- flake.nix
+++ flake.nix
@@ -1,20 +1,57 @@
 {
-  outputs = { self, nixpkgs }: let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
-  in
-  {
-    devShells.x86_64-linux.default = pkgs.mkShell rec {
-
-      buildInputs = [
-        pkgs.pipenv
-        pkgs.zlib
-      ];
-
-      shellHook = ''
-        export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath buildInputs}:$LD_LIBRARY_PATH"
-        export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib.outPath}/lib:$LD_LIBRARY_PATH"
-      '';
-
-    };
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
   };
+
+  outputs = { self, nixpkgs, flake-utils }: 
+    flake-utils.lib.eachDefaultSystem (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config = { allowUnfree = true; };
+          };
+        in {
+          devShell = pkgs.mkShell rec {
+
+            buildInputs = with pkgs; [
+              pipenv
+              zlib
+
+              autoconf
+              binutils
+              curl
+              freeglut
+              git
+              gitRepo
+              gnumake
+              gnupg
+              gperf
+              # libGLU_combined
+              # linuxPackages.nvidia_x11
+              m4
+              ncurses5
+              procps
+              stdenv.cc
+              unzip
+              utillinux
+              xorg.libX11
+              xorg.libXext
+              xorg.libXi
+              xorg.libXmu
+              xorg.libXrandr
+              xorg.libXv
+              zlib
+            ];
+
+            shellHook = ''
+              export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath buildInputs}:$LD_LIBRARY_PATH"
+              export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib.outPath}/lib:$LD_LIBRARY_PATH"
+
+              # export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
+              # export EXTRA_CCFLAGS="-I/usr/include"
+            '';
+
+          };
+        }
+      );
 }
